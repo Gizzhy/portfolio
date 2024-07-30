b8ac55a7b9deee021e508efd257588811c34a19a
Gizzhy <gizzhyakinz@gmail.com>
2024-07-31 00:17:13 +0100

    projects homepage

--- src/pages/home/Body.jsx
+++ src/pages/home/Body.jsx
@@ -1,11 +1,12 @@
 import "./styles/body.css";
 import styles from "./styles/body.module.scss";
-import travel from "../../assets/travel.png";
-import simi from "../../assets/simi.png";
+import strart from "../../assets/strart.png";
+import simi from "../../assets/simiii.png";
 import codesystic from "../../assets/codesystic1.png";
 import isokan from "../../assets/isokan1.png";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import { IoArrowRedoSharp } from "react-icons/io5";
 
 const Body = () => {
   return (
@@ -27,7 +28,7 @@ const Body = () => {
             />
           </div>
           <div className={styles.projectSecond3}>
-            <p className={styles.projectWords}>Codesystic</p>
+            <p className={styles.projectWordss}>Codesystic</p>
             <p className={styles.projectWords}>
               Information Technology Solutions provider. We provide
               cost-effective services on Mobile app Development, Ui/Ux Designs,
@@ -43,7 +44,7 @@ const Body = () => {
         </div>
         <div className={styles.projectSecond}>
           <div className={styles.projectSecond2}>
-            <p className={styles.projectWords}>Egbe Isokan Yoruba Germany</p>
+            <p className={styles.projectWordss}>Egbe Isokan Yoruba Germany</p>
             <p className={styles.projectWords}>
               We are non-governmental, non-profit, non-political, and
               non-religious organisation to foster Socio-cul-tural development
@@ -65,36 +66,58 @@ const Body = () => {
             />
           </div>
         </div>
-      </div>
-      {/* <div className="akoko3">
-        <p className="exp">Projects</p>
-      </div>
-      <div className="proj-area">
-        
-          <div className="proj1-2">
-            <img src={simi} alt="codesystic" className="proj-img" />
-            <p className="proj-oro">Diffshadesofsimmie</p>
-            <p className="proj-oro2">
+        <div className={styles.projectSecond}>
+          <div className={styles.projectSecond2}>
+            <img
+              src={strart}
+              className={styles.projectImage}
+              alt="codesystic"
+            />
+          </div>
+          <div className={styles.projectSecond3}>
+            <p className={styles.projectWordss}>Strart</p>
+            <p className={styles.projectWords}>
+              Information Technology Solutions provider. We provide
+              cost-effective services on Mobile app Development, Ui/Ux Designs,
+              Responsive Web Design, BackEnd Development, Graphics Designs,
+              Crypto-Web3 insights & Investments.
+            </p>
+            <p className={styles.projectWords}>
+              <span className={styles.projectSpan}>STACK: </span>React, NEXTjs,
+              Tailwind
+            </p>
+            <button className={styles.projectButton}>View Git Repo</button>
+          </div>
+        </div>
+        <div className={styles.projectSecond}>
+          <div className={styles.projectSecond2}>
+            <p className={styles.projectWordss}>Shades of Simi</p>
+            <p className={styles.projectWords}>
               Discover our curated collection of trendy eyewear designed to
               elevate your look and protect your eyes. Find your perfect pair
               today and step into a world of clarity and fashion
             </p>
+            <p className={styles.projectWords}>
+              <span className={styles.projectSpan}>STACK: </span>React
+            </p>
+            <button className={styles.projectButton}>View Git Repo</button>
+          </div>
+          <div className={styles.projectSecond3}>
+            <img
+              src={simi}
+              className={styles.projectImage}
+              alt="shades of simi"
+            />
           </div>
         </div>
-        <div className="proj2">
-          <div className="proj1-1">
-            <img src={yoruba} alt="codesystic" className="proj-img" />
-            <p className="proj-oro">Egbe Isokan Yoruba Germany</p>
-            <p className="proj-oro2">
-              We are non-governmental, non-profit, non-political, and
-              non-religious organisation to foster Socio-cul-tural development
-              of the Yoruba race in Diaspora. The association aims at producing
-              an atmosphere for Yoruba’s and other interested people to share
-              and discover Yoruba culture
-            </p>
-          </div>
-          <div className="proj1-2">
-            <img src={travel} alt="codesystic" className="proj-img" />
+        <div className={styles}></div>
+        <button className={styles.projectButton2}>
+          View all <IoArrowRedoSharp />
+        </button>
+      </div>
+      {/* <div className="akoko3">
+        <p className="exp">Projects</p>
+      </div>
             <p className="proj-oro">Travel Wings</p>
             <p className="proj-oro2">
               Embark on your next adventure with us at TravelWings. Explore
